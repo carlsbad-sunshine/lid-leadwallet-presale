78da4600744274e11ca11f63388f5886037910ca
Carlsbad <carlsbad_sunshine@protonmail.com>
2020-09-03 00:38:38 -0700

    Initial commit

---
+++ src/contracts/addresses.js
@@ -0,0 +1,10 @@
+const addresses = {
+  presale: '0x321FF0906619DeCfaCE3ff81C0FeB9ab0D001EEE',
+  redeemer: '0x7b0F99f9E4AA9fAea083aE72A63962bFb373C773',
+  timer: '0x261616dfc1a9736cC41134Ca86C214027C767570',
+  token: '0x1dd80016e3d4ae146ee2ebb484e8edd92dacc4ce',
+  access: '0xfD8e59814D601219bddd53879ADa1Ff75fD316e2',
+  staking: '0x9a54FE35D41bc7c8F7071aBf0cCD952505e29ceb'
+};
+
+export default addresses;
